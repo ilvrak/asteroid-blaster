08053436620f6895bf8b28d3bea377da87920811
Ilvrak <ilvrak@gmail.com>
2023-09-09 14:39:53 +0800

    asteroids and laser

--- asteroid-blaster.py
+++ asteroid-blaster.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 
 pygame.init()
 
@@ -24,6 +25,19 @@ rotation_speed = 1
 # Количество пикселей, на которое корабль сдвигается по X за одну итерацию
 move_increment = 0.01
 
+# Параметры лазера
+laser_width = 1
+laser_color = 'red'
+
+# Генерация астероидов
+asteroid_radius_range = (5, 10)
+asteroids = []
+for _ in range(100):
+    asteroid_radius = random.randint(*asteroid_radius_range)
+    asteroid_x = random.randint(0, screen_width)
+    asteroid_y = random.randint(0, screen_height)
+    asteroids.append((asteroid_x, asteroid_y, asteroid_radius))
+
 
 
 # Основной цикл программы
@@ -37,11 +51,13 @@ while running:
     # Очистка экрана
     screen.fill('black')
 
+
     # Рисование и позиционирование космического корабля
+    ship_nose = (ship_x + ship_length, ship_y + ship_width // 2)
     points = [
         (ship_x, ship_y),
         (ship_x, ship_y + ship_width),
-        (ship_x + ship_length, ship_y + ship_width // 2)
+        ship_nose
     ]
 
     pygame.draw.polygon(screen, 'white', points, 1)
@@ -52,10 +68,29 @@ while running:
 
     # Если корабль заходит за правый край экрана, возвращаем его в начало
     if ship_x > screen_width:
-        ship_x = 0
+        running = False
+
+
+    # Рисование астероидов
+    for asteroid in asteroids:
+        asteroid_x, asteroid_y, asteroid_radius = asteroid
+        pygame.draw.circle(screen, 'darkgray', (asteroid_x, asteroid_y), asteroid_radius, 1)
+
+
+    # Рисование лазера и проверка столкновения с астероидами
+    for asteroid in asteroids:
+        asteroid_x, asteroid_y, asteroid_radius = asteroid
+
+        # Проверка, расстояние от корабля до астероида
+        distance = math.hypot(asteroid_x - ship_x, asteroid_y - ship_y)
+        if distance < 100:
+            # Рисование лазера
+            pygame.draw.line(screen, laser_color, ship_nose, (asteroid_x, asteroid_y), laser_width)
+            asteroids.remove(asteroid)
+
 
     # Обновление экрана
-    pygame.display.flip()    
+    pygame.display.flip()
 
 # Завершение программы
 pygame.quit()
